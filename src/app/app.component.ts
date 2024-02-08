3e2bf5a88479eb71d0aed63a6df0693e5bd56fa0
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-02-08 09:40:48 -0300

    Montando o cabeçalho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [CommonModule, RouterOutlet, ContainerComponent, CabecalhoComponent],
 })
 export class AppComponent {
   title = 'indexa';
